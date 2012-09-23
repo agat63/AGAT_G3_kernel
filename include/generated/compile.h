05563b844db1616ba6e9f5312ed72b4132eec35f
agat63 <xta0407@gmail.com>
2012-09-22 18:58:53 -0700

    Initial commit. Samsung GS3 source update 1

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jul 23 20:55:22 KST 2012"
+#define LINUX_COMPILE_BY "shina.lee"
+#define LINUX_COMPILE_HOST "Sprint10"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
